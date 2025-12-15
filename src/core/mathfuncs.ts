49af9c4012c6d1a5253fbaef668e72b00769a638
crosscountrycoder <sanjay.k.srikanth@gmail.com>
2025-12-14 21:14:21 -0800

    more accurate formula for delta T, especially in recent past and near future

--- src/core/mathfuncs.ts
+++ src/core/mathfuncs.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon';
-import {DAY_LENGTH, earthERadius, flattening, J2000UTC, degToRad} from "./constants.ts";
+import {DAY_LENGTH, earthERadius, flattening, J2000UTC, degToRad, deltaT} from "./constants.ts";
 
 export type Point = [number, number];
 export type Polygon = Point[];
@@ -27,70 +27,22 @@ export function refract(elev: number): number {
     return elev + refraction;
 }
 
-/** This function finds the approximate value of ΔT, which is calculated using the formula ΔT = TT - UT1. 
+/** This function finds the approximate value of ΔT, which is calculated using the formula ΔT = TT - UT1. TT is terrestrial time
+ * (based on atomic clocks) and UT1 is mean solar time at 0° longitude.
  * 
- * TT is terrestrial time (based on atomic clocks) and UT1 is mean solar time at 0° longitude.
- * 
- * This function's margin of error is 4.8 seconds in 2024, based on the value this function returns (73.8 seconds) versus the
- * real value (69 seconds). The margin of error increases for years before 1800 and after 2100, as the Earth's rotation 
- * varies unpredictably.
- * 
- * Source: https://eclipse.gsfc.nasa.gov/SEhelp/deltatpoly2004.html
  * @param t Unix time in milliseconds. */
 export function approxDeltaT(t: number) {
     const y = fractionalYear(t);
-    if (y < 500) {
-        const u = y/100;
-        return 10583.6 - 1014.41 * u + 33.78311 * u**2 - 5.952053 * u**3 - 0.1798452 * u**4 + 0.022174192 * u**5 + 0.0090316521 * u**6;
-    }
-    else if (y < 1600) {
-        const u = (y-1000)/100;
-        return 1574.2 - 556.01*u + 71.23472*u**2 + 0.319781*u**3 - 0.853463*u**4 - 0.005050998*u**5 + 0.0083572073*u**6;
-    }
-    else if (y < 1700) {
-        const t = y - 1600;
-        return 120 - 0.9808*t - 0.01532*t**2 + t**3/7129;
-    }
-    else if (y < 1800) {
-        const t = y - 1700;
-        return 8.83 + 0.1603*t - 0.0059285*t**2 + 0.00013336*t**3 - t**4/1174000;
-    }
-    else if (y < 1860) {
-        const t = y - 1800;
-        return 13.72 - 0.332447 * t + 0.0068612 * t**2 + 0.0041116 * t**3 - 0.00037436 * t**4 + 0.0000121272 * t**5 - 0.0000001699 * t**6 + 0.000000000875 * t**7;
-    }
-    else if (y < 1900) {
-        const t = y - 1860;
-        return 7.62 + 0.5737 * t - 0.251754 * t**2 + 0.01680668 * t**3 - 0.0004473624 * t**4 + t**5 / 233174;
-    }
-    else if (y < 1920) {
-        const t = y - 1900;
-        return -2.79 + 1.494119 * t - 0.0598939 * t**2 + 0.0061966 * t**3 - 0.000197 * t**4;
-    }
-    else if (y < 1941) {
-        const t = y - 1920;
-        return 21.20 + 0.84493*t - 0.076100 * t**2 + 0.0020936 * t**3;
-    }
-    else if (y < 1961) {
-        const t = y - 1950;
-        return 29.07 + 0.407*t - t**2/233 + t**3 / 2547;
-    }
-    else if (y < 1986) {
-        const t = y - 1975;
-        return 45.45 + 1.067*t - t**2/260 - t**3 / 718;
-    }
-    else if (y < 2005) {
-        const t = y - 2000;
-        return 63.86 + 0.3345 * t - 0.060374 * t**2 + 0.0017275 * t**3 + 0.000651814 * t**4 + 0.00002373599 * t**5;
-    }
-    else if (y < 2050) {
-        const t = y - 2000;
-        return 62.92 + 0.32217 * t + 0.005589 * t**2;
-    }
-    else if (y < 2150) {return 32 * ((y-1820)/100)**2 - 0.5628 * (2150 - y) - 20;}
+    if (y <= deltaT[0][0]) {return deltaT[0][1];}
     else {
-        const u = (y-1820)/100;
-        return 32*u**2-20;
+        for (let i=0; i<deltaT.length-1; i++) {
+            if (deltaT[i][0] <= y && deltaT[i+1][0] >= y) {
+                const frac = (y - deltaT[i][0]) / (deltaT[i+1][0] - deltaT[i][0]);
+                const diff = deltaT[i+1][1] - deltaT[i][1];
+                return deltaT[i][1] + diff * frac;
+            }
+        }
+        return deltaT[deltaT.length-1][1];
     }
 }
 
